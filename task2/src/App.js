42dda3078212b2248112b6065f07e9512d6c81f1
Erlina Cico <erlinacico@gmail.com>
2025-07-15 00:26:31 +0200

    fetch data from API

---
+++ task2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserTable from './components/UserTable';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>User Directory</h1>
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
